c9cb6874d4b257f43df5b9454767700d05797c21
ViniciusSTN <vinicius.stn@gmail.com>
2024-06-11 11:59:55 -0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Routes,  } from "react-router-dom"
 import { Home } from "./pages/Home"
-import { Header } from "./components/Header"
+
 import { Footer } from "./components/Footer"
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
-      <Header />
       <BrowserRouter>
+        <Header />
+
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        
+        <Footer />
       </BrowserRouter>
-      <Footer />
     </>
   )
 }
