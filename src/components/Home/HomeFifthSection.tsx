67956b1a0e0e22aeaa38a713eda22afcc08601e4
ViniciusSTN <vinicius.stn@gmail.com>
2024-06-12 15:53:41 -0300

    feat: add home-fifth-section component

---
+++ src/components/Home/HomeFifthSection.tsx
@@ -0,0 +1,11 @@
+export const HomeFifthSection = () => {
+  return (
+    <section className="container mx-auto mb-12">
+      <div className="md:relative md:top-8">
+        <p className="font-Poppins font-semibold text-xl text-gray200 text-center">Share your setup with</p>
+        <h2 className="font-Poppins font-bold text-4.5xl text-darkgreen text-center">#FuniroFurniture</h2>
+      </div>
+      <img src="https://project3-images-storage.s3.us-east-2.amazonaws.com/static/mosaico.png" alt="setups" />
+    </section>
+  )
+}
